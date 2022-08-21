cf6e84e752e166a5881b6193c6fbcb1307d708a3
DuskyElf <clayedison75@gmail.com>
2022-08-21 14:00:16 +0530

    Added feature to show the distribution

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ use rand::{self, Rng};
 const ITTERATIONS: u64 = 1_000_000;
 
 fn main() {
-    let mut sum = 0;
+    let mut sum = 0u64;
+    let mut distribution = [0; 10];
+
     for _ in 0..ITTERATIONS {
-        let result = frog_simulation() as u64;
-        sum += result;
+        let result = frog_simulation();
+        sum += result as u64;
+
+        distribution[(result - 1) as usize] += 1;
     }
 
     println!("Average: {}", sum as f64 / ITTERATIONS as f64);
+    println!("Distributions -> {:?}", distribution);
 }
 
 fn frog_simulation() -> u8 {
